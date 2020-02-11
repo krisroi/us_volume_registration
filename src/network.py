d6c4a630f8ecec09f05d2818a46b2e463c13e08e
Kristoffer Roise <kristoffer.roise@outlook.com>
2020-02-11 14:10:36 +0100

    Added affine regression and combined with DenseNet

--- src/network.py
+++ src/network.py
@@ -109,15 +109,14 @@ class _DenseNet(nn.Module):
         bn_size (int) - multiplicative factor for number of bottle neck layers
           (i.e. bn_size * k features in the bottleneck layer)
         drop_rate (float) - dropout rate after each dense layer
-        num_classes (int) - number of classification classes
         memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
           but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
     """
 
     __constants__ = ['features']
 
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000, memory_efficient=False):
+    def __init__(self, growth_rate=24, block_config=(1, 2, 4, 8, 16),
+                 num_init_features=8, bn_size=4, drop_rate=0, memory_efficient=True):
 
         super(_DenseNet, self).__init__()
 
@@ -131,6 +130,7 @@ class _DenseNet(nn.Module):
         ]))
 
         # Each denseblock
+        # block_config = (1,2,3,4) yields 4 blocks, with 1, 2, 3 and 4 layers respectively
         num_features = num_init_features
         for i, num_layers in enumerate(block_config):
             block = _DenseBlock(
@@ -151,93 +151,48 @@ class _DenseNet(nn.Module):
 
         # Final batch norm
         self.features.add_module('norm5', nn.BatchNorm3d(num_features))
-        
-        self.classifier = nn.Linear(num_features, num_classes)
 
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
         out = F.relu(features, inplace=True)
-        out = F.adaptive_avg_pool3d(out, (1, 1, 1))
-        out = torch.flatten(out, 1)
-        out = self.classifier(out)
+        #out = F.adaptive_avg_pool3d(out, (1, 1, 1))
         return out
-    
-def _densenet(arch, growth_rate, block_config, num_init_features, pretrained, progress,
-              **kwargs):
-    model = _DenseNet(growth_rate, block_config, num_init_features, **kwargs)
-    if pretrained:
-        _load_state_dict(model, model_urls[arch], progress)
-    return model
-
-def _airnet(arch, growth_rate, block_config, num_init_features, pretrained, progress, **kwargs):
-    
 
 
-def densenet121(pretrained=False, progress=True, **kwargs):
-    r"""Densenet-121 model from
-    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-        memory_efficient (bool) - If True, uses checkpointing. Much more memory efficient,
-          but slower. Default: *False*. See `"paper" <https://arxiv.org/pdf/1707.06990.pdf>`_
-    """
-    return _densenet('densenet121', 12, (6, 10, 8), 1, pretrained, progress,
-                     **kwargs)
+class _AffineRegression(nn.Module):
+    def __init__(self):
+        super(_AffineRegression, self).__init__()
+
+        # Regression
+        self.params = nn.Sequential(OrderedDict([
+            ('fc0', nn.Linear(1024, 512)),
+            ('norm0', nn.BatchNorm1d(512)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('fc1', nn.Linear(512, 256)),
+            ('norm1', nn.BatchNorm1d(256)),
+            ('relu1', nn.ReLU(inplace=True)),
+            ('fc2', nn.Linear(256, 128)),
+            ('norm2', nn.BatchNorm1d(128)),
+            ('relu2', nn.ReLU(inplace=True)),
+            ('fc3', nn.Linear(128, 12))
+        ]))
+
+        # Initializing last fully connected layer to the identity matrix
+        self.params.fc3.weight.data.zero_()
+        self.params.fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], dtype=torch.float64))
+
+    def forward(self, x):
+        out = self.params(x)
+        return out
 
 
-class _Regressor(nn.Sequential):
-    def __init__(self, num_input_features, num_theta_parameters, reduction):
-        super(_Regressor, self).__init__()
-        self.add_module('fc0', nn.Linear(num_input_features, num_input_features // reduction))
-        self.add_module('norm0', nn.BatchNorm1d(num_input_features // reduction))
-        self.add_module('relu0', nn.ReLU(inplace=True))
-        self.add_module('fc1', nn.Linear(num_input_features // reduction, num_input_features // (2 * reduction)))
-        self.add_module('norm1', nn.BatchNorm1d(num_input_features // (2 * reduction)))
-        self.add_module('relu1', nn.ReLU(inplace=True))
-        self.add_module('fc2', nn.Linear(num_input_features // (2 * reduction), num_input_features // (8 * reduction)))
-        self.add_module('norm2', nn.BatchNorm1d(num_input_features // (8 * reduction)))
-        self.add_module('relu2', nn.ReLU(inplace=True))
-        self.add_module('fc3', nn.Linear(num_input_features // (8 * reduction), num_theta_parameters))
-
-        #self.fc3.weight.data.zero_()
-        #self.fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], dtype=torch.float64))
-
-'''
 class USAIRNet(nn.Module):
-
-    __constants__ = ['fixed_loc, moving_loc']
-
-    def __init__(self, growth_rate=4, block_config=(2, 6, 12, 8),
-                 num_init_features=1, bn_size=2, drop_rate=0, memory_efficient=False,
-                 num_input_features=43218, num_theta_parameters=12, reduction=8):
-
+    def __init__(self, denseNet, affineRegression):
         super(USAIRNet, self).__init__()
-        
-        self.fixed_loc = nn.Sequential()
-        self.moving_loc = nn.Sequential()
-        self.regressor = nn.Sequential()
-        
-        dense = _DenseNet(growth_rate=growth_rate,
-                          block_config=block_config,
-                          num_init_features=num_init_features,
-                          bn_size=bn_size,
-                          drop_rate=drop_rate,
-                          memory_efficient=memory_efficient
-                          )
-        
-        self.fixed_loc.add_module('densenet%d', dense)
-        self.moving_loc.add_module('densenet%d', dense)
 
-        self.fixed_loc.add_module('flatten', nn.Flatten())
-        self.moving_loc.add_module('flatten', nn.Flatten())
-
-        regress = _Regressor(num_input_features=num_input_features,
-                             num_theta_parameters=num_theta_parameters,
-                             reduction=reduction)
-        
-        self.regressor.add_module('regressor%d', regress)
+        self.denseNet = denseNet
+        self.affineRegression = affineRegression
 
         # Official init from torch repo.
         for m in self.modules():
@@ -246,21 +201,43 @@ class USAIRNet(nn.Module):
             elif isinstance(m, nn.BatchNorm3d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, fixed, moving):
-        fixed = self.fixed_loc(fixed)
-        moving = self.moving_loc(moving)
-        concated_params = torch.cat((fixed, moving), 1)
-        theta = self.regressor(concated_params)
+        fixed_loc = self.denseNet(fixed)
+        moving_loc = self.denseNet(moving)
+        print(fixed_loc.shape)
+        fixed_loc = torch.flatten(fixed_loc, 1)
+        moving_loc = torch.flatten(moving_loc, 1)
+        concated = torch.cat((fixed_loc, moving_loc), 1)
+        theta = self.affineRegression(concated)
+        theta = theta.view(-1, 3, 4)
         return theta
-'''
+
+
+# Utility for counting parameters
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
 if __name__ == '__main__':
 
-    net = densenet121()
-    
-    randn = torch.randn(1, 1, 224, 224, 224)
-    
+    denseNet = _DenseNet(growth_rate=24, block_config=(1, 2, 4, 8, 16),
+                         num_init_features=8, bn_size=4, drop_rate=0,
+                         memory_efficient=True
+                         )
+    affineRegression = _AffineRegression()
+
+    net = USAIRNet(denseNet, affineRegression)
+
+    dim = 70
+    batch_size = 4
+
+    fixed = torch.randn(batch_size, 1, dim, dim, dim)
+    moving = torch.randn(batch_size, 1, dim, dim, dim)
+
     print(net)
-    print(net(randn).shape)
+
+    net(fixed, moving)
