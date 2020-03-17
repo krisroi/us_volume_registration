d3a94d77934bdca51bde94ce7009477c1d18e0bb
Kristoffer Roise <kristoffer.roise@outlook.com>
2020-03-17 14:44:01 +0100

    Removed the last conv layer in the model

--- src/models/PLSNet_Encoder.py
+++ src/models/PLSNet_Encoder.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import itertools
+
 
 class _DSConv(nn.Module):
     def __init__(self, num_input_features, growth_rate, padding, dilation):
@@ -84,13 +86,13 @@ class _PLSNet(nn.Module):
 
     __constants__ = ['features']
 
-    def __init__(self, PLS_CONFIG, growth_rate, num_init_features):
+    def __init__(self, encoder_config, growth_rate, num_init_features):
         super(_PLSNet, self).__init__()
 
-        self.PLS_CONFIG = PLS_CONFIG
+        self.encoder_config = encoder_config
 
         self.drd_module = nn.ModuleDict()
-        self.strided_ds_module = nn.ModuleDict()
+        self.strided_conv_module = nn.ModuleDict()
 
         # Initial StridedDSConv
         self.add_module('conv0', nn.Conv3d(1, 1, kernel_size=3, stride=1, padding=1,
@@ -101,64 +103,32 @@ class _PLSNet(nn.Module):
         self.add_module('relu', nn.ReLU(inplace=True))
 
         num_features = num_init_features + 1
-        for i, num_layers in enumerate(PLS_CONFIG):
+        for i, num_layers in enumerate(encoder_config):
             DRD_BLOCK = _DilatedResidualDenseBlock(num_layers=num_layers,
                                                    num_input_features=num_features,
                                                    growth_rate=growth_rate)
             self.drd_module.add_module('DRD_BLOCK%d' % (i + 1), DRD_BLOCK)
 
-            STRIDED_DS_LAYER = _StridedDSConv(num_input_features=num_features,
-                                              num_output_features=num_init_features * (2**(i + 1)))
-            self.strided_ds_module.add_module('DS_LAYER%d' % (i + 1), STRIDED_DS_LAYER)
+            if i != len(encoder_config) - 1:
+
+                STRIDED_CONV_LAYER = _StridedDSConv(num_input_features=num_features,
+                                                    num_output_features=num_init_features * (2**(i + 1)))
+                self.strided_conv_module.add_module('DS_LAYER%d' % (i + 1), STRIDED_CONV_LAYER)
             num_features = num_init_features * (2**(i + 1)) + 1
 
     def forward(self, x):
         origInput = x
         out = self.relu(self.norm(self.conv1(self.conv0(x))))
-        print('Out shape after first DS:    ', out.shape)
 
-        for i, ((_, DRD_BLOCK), (_, STRIDED_DS_LAYER)) in enumerate(zip(self.drd_module.items(),
-                                                                        self.strided_ds_module.items())):
-            print('____LAYER NUMBER {}____'.format(i + 1))
-            downsampled_data = F.interpolate(origInput, scale_factor=(1 / (2 ** (i + 1))),
+        for i, ((DRD_NAME, DRD_BLOCK), (STRIDED_CONV_NAME, STRIDED_CONV_LAYER)) in \
+                enumerate(itertools.zip_longest(self.drd_module.items(), self.strided_conv_module.items(),
+                                                fillvalue=(0, 'placeholder'))):
+            downsampled_data = F.interpolate(input=origInput,
+                                             scale_factor=(1 / (2 ** (i + 1))),
                                              mode='trilinear')
-            print('Downsapled data shape:       ', downsampled_data.shape)
             out = torch.cat((out, downsampled_data), 1)
-            print('Out posterior to concat:     ', out.shape)
             out = DRD_BLOCK(out)
-            print('Out after DRD_BLOCK:         ', out.shape)
-            if i != len(self.PLS_CONFIG) - 1:
-                out = STRIDED_DS_LAYER(out)
-                print('Out after StridedDSConv:     ', out.shape)
+            if i != len(self.encoder_config) - 1:
+                out = STRIDED_CONV_LAYER(out)
         out = F.relu(out, inplace=True)
         return out
-
-
-if __name__ == '__main__':
-
-    # Supress warnings
-    import warnings
-    warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.functional")
-
-    arr = torch.randn(4, 1, 64, 64, 64)
-
-    # net1 = _DSConv(num_input_features=1, growth_rate=12, dilation=2)
-    # net2 = _StridedDSConv(num_input_features=1, num_output_features=12)
-    # net3 = _DilatedResidualDenseBlock(num_layers=4, num_input_features=8, growth_rate=12)
-    # net4 = _InputReinforcement(layerNum=1)
-
-    # DS = net2(array)
-    # print(DS.shape)
-    # IR = net4(array)
-    # print(IR.shape)
-    # out = torch.cat((DS, IR), 1)
-    # print(out.shape)
-
-    # print(net1)
-    # print(net2)
-    # print(net3)
-
-    PLS_CONFIG = (4, 4, 4, 4)
-
-    net5 = _PLSNet(PLS_CONFIG=PLS_CONFIG, growth_rate=12, num_init_features=8)
-    net5(arr)
