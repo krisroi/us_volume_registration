ae4aee920bc735b105884e8e6f42385c005f1032
Kristoffer Roise <kristoffer.roise@outlook.com>
2020-04-29 09:43:30 +0200

    Added func to return number of folds for CV

--- src/utils/parse_utils.py
+++ src/utils/parse_utils.py
@@ -40,8 +40,8 @@ def range_limited_float_type(arg):
 def range_limited_int_type_TOT_NUM_SETS(arg):
     """Type function for argparse - an integer within some predefined bounds"""
     i = int_type(arg)
-    if i < 1 or i > 25:
-        raise argparse.ArgumentTypeError('Total number of sets must be between {} and {}'.format(1, 25))
+    if i < 1 or i > 23:
+        raise argparse.ArgumentTypeError('Total number of sets must be between {} and {}'.format(1, 23))
     return i
 
 
