6431f80bd9bc9dd3894853e99c0b96c01beb5f99
Kristoffer Roise <kristoffer.roise@outlook.com>
2020-05-11 10:30:38 +0200

    Added helper function to visualize NCC mask during training

--- src/utils/utility_functions.py
+++ src/utils/utility_functions.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('tkagg')
 
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import math
 import torch
 from utils.affine_transform import affine_transform
@@ -193,3 +194,89 @@ def plotPatchwisePrediction(fixed_batch, moving_batch, predicted_theta, PROJ_ROO
     # if savefig:
     #    output_dir = os.path.join(PROJ_ROOT, PROJ_NAME, 'output/predictions/')
     #    plt.savefig('{}patch_predictionss.png'.format(output_dir), dpi=225, format='png', bbox_inches='tight', pad_inches=0)
+
+
+def plotTrainPredictions(fixed_batch, moving_batch, predicted_theta, mask, PROJ_ROOT, PROJ_NAME, savefig=False, copperAlpha=1, grayAlpha=0.6):
+    batch_size = fixed_batch.shape[0]
+    warped_batch = affine_transform(moving_batch, predicted_theta)
+
+    fixed_batch = fixed_batch.detach().numpy()
+    moving_batch = moving_batch.detach().numpy()
+    warped_batch = warped_batch.detach().numpy()
+    mask = mask.detach().numpy()
+
+    x_range = batch_size
+    y_range = 2
+
+    fig_x, ax_x = plt.subplots(y_range, x_range, squeeze=False, figsize=(40, 40))
+    fig_y, ax_y = plt.subplots(y_range, x_range, squeeze=False, figsize=(40, 40))
+    fig_z, ax_z = plt.subplots(y_range, x_range, squeeze=False, figsize=(40, 40))
+
+    count = 0
+    for i in range(y_range):
+        for j in range(x_range):
+            ax_x[i, j].get_xaxis().set_visible(False)
+            ax_x[i, j].get_yaxis().set_visible(False)
+            ax_y[i, j].get_xaxis().set_visible(False)
+            ax_y[i, j].get_yaxis().set_visible(False)
+            ax_z[i, j].get_xaxis().set_visible(False)
+            ax_z[i, j].get_yaxis().set_visible(False)
+
+            ax_x[i, j].set_xlim([0, fixed_batch.shape[2]])
+            ax_x[i, j].set_ylim([fixed_batch.shape[2], 0])
+            ax_y[i, j].set_xlim([0, fixed_batch.shape[2]])
+            ax_y[i, j].set_ylim([fixed_batch.shape[2], 0])
+            ax_z[i, j].set_xlim([0, fixed_batch.shape[2]])
+            ax_z[i, j].set_ylim([fixed_batch.shape[2], 0])
+
+            if count != batch_size:
+                fixed_x = fixed_batch[count, 0, fixed_batch.shape[2] // 2]
+                fixed_y = fixed_batch[count, 0, :, fixed_batch.shape[3] // 2]
+                fixed_z = fixed_batch[count, 0, :, :, fixed_batch.shape[4] // 2]
+
+                moving_x = moving_batch[count, 0, moving_batch.shape[2] // 2]
+                moving_y = moving_batch[count, 0, :, moving_batch.shape[3] // 2]
+                moving_z = moving_batch[count, 0, :, :, moving_batch.shape[4] // 2]
+
+                warped_x = warped_batch[count, 0, warped_batch.shape[2] // 2]
+                warped_y = warped_batch[count, 0, :, warped_batch.shape[3] // 2]
+                warped_z = warped_batch[count, 0, :, :, warped_batch.shape[4] // 2]
+
+                mask_x = mask[count, 0, mask.shape[2] // 2]
+                mask_y = mask[count, 0, :, mask.shape[3] // 2]
+                mask_z = mask[count, 0, :, :, mask.shape[4] // 2]
+
+                # Plot x-slixed predictions
+                ax_x[0, j].imshow(fixed_x, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_x[0, j].imshow(moving_x, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_x[1, j].imshow(fixed_x, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_x[1, j].imshow(warped_x, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_x[0, j].title.set_text('No alignment')
+                ax_x[1, j].title.set_text('Predicted alignment')
+
+                # Plot y-slixed predictions
+                ax_y[0, j].imshow(fixed_y, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_y[0, j].imshow(moving_y, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_y[1, j].imshow(fixed_y, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_y[1, j].imshow(warped_y, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_y[0, j].title.set_text('No alignment')
+                ax_y[1, j].title.set_text('Predicted alignment')
+
+                # Plot z-slixed predictions
+                ax_z[0, j].imshow(fixed_z, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_z[0, j].imshow(moving_z, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_z[1, j].imshow(fixed_z, origin='left', cmap='copper', alpha=copperAlpha)
+                ax_z[1, j].imshow(warped_z, origin='lef', cmap='gray', alpha=grayAlpha)
+                ax_z[0, j].title.set_text('No alignment')
+                ax_z[1, j].title.set_text('Predicted alignment')
+
+                ax_x[1, j].imshow(mask_x, origin='left', cmap='cool', alpha=0.1)
+                ax_y[1, j].imshow(mask_y, origin='left', cmap='cool', alpha=0.1)
+                ax_z[1, j].imshow(mask_z, origin='left', cmap='cool', alpha=0.1)
+
+                count += 1
+
+    fig_x.suptitle('x-sliced patchwise predictions for batch_size {}'.format(batch_size))
+    fig_y.suptitle('y-sliced patchwise predictions for batch_size {}'.format(batch_size))
+    fig_z.suptitle('z-sliced patchwise predictions for batch_size {}'.format(batch_size))
+    plt.show()
